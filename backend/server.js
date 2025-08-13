5e691e7b3b52710ddb721df4c7f42a2f3b7954e3
tomsecundo <n12300136@qut.edu.au>
2025-08-13 23:46:48 +1000

    updated db.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
